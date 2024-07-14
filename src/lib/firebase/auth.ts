4960119476b6977a6d81c2c02e1747af3b6a1ff4
FiveEightyEight <robert.abreu3@gmail.com>
2024-07-13 21:08:11 -0400

    update paths

--- src/lib/firebase/auth.ts
+++ src/lib/firebase/auth.ts
@@ -4,16 +4,21 @@ import {
     onAuthStateChanged as _onAuthStateChanged,
 } from "firebase/auth";
 
-import { auth } from "@/src/lib/firebase/clientApp";
-
-export function onAuthStateChanged(cb) {
-    return () => { };
-}
+import { auth } from "@/lib/firebase/clientApp";
 
 export async function signInWithGoogle() {
-    return;
+    const provider = new GoogleAuthProvider();
+    try {
+        await signInWithPopup(auth, provider);
+    } catch (error) {
+        console.error("Error signing in with Google", error);
+    }
 }
 
 export async function signOut() {
-    return;
+    try {
+        return auth.signOut();
+    } catch (error) {
+        console.error("Error signing out with Google", error);
+    }
 }
