63417733572141224bcf11b19e1e1228856a5dbd
FiveEightyEight <robert.abreu3@gmail.com>
2024-07-14 12:40:01 -0400

    minor import name fix

--- src/components/ChoreItem.tsx
+++ src/components/ChoreItem.tsx
@@ -12,7 +12,7 @@ const projects = [
         name: 'GraphQL API',
         href: '#',
         status: 'Complete',
-        createdBy: 'Leslie Alexander',
+        createdBy: 'CJ',
         dueDate: 'March 17, 2023',
         dueDateTime: '2023-03-17T00:00Z',
     },
@@ -21,7 +21,7 @@ const projects = [
         name: 'New benefits plan',
         href: '#',
         status: 'In progress',
-        createdBy: 'Leslie Alexander',
+        createdBy: 'CJ',
         dueDate: 'May 5, 2023',
         dueDateTime: '2023-05-05T00:00Z',
     },
@@ -30,7 +30,7 @@ const projects = [
         name: 'Onboarding emails',
         href: '#',
         status: 'In progress',
-        createdBy: 'Courtney Henry',
+        createdBy: 'CJ',
         dueDate: 'May 25, 2023',
         dueDateTime: '2023-05-25T00:00Z',
     },
@@ -39,7 +39,7 @@ const projects = [
         name: 'iOS app',
         href: '#',
         status: 'In progress',
-        createdBy: 'Leonard Krasner',
+        createdBy: 'CJ',
         dueDate: 'June 7, 2023',
         dueDateTime: '2023-06-07T00:00Z',
     },
@@ -48,7 +48,7 @@ const projects = [
         name: 'Marketing site redesign',
         href: '#',
         status: 'Archived',
-        createdBy: 'Courtney Henry',
+        createdBy: 'CJ',
         dueDate: 'June 10, 2023',
         dueDateTime: '2023-06-10T00:00Z',
     },
@@ -58,7 +58,7 @@ function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(' ')
 }
 
-export default function Example() {
+export default function ChoreItems() {
     return (
         <ul role="list" className="divide-y divide-gray-100">
             {projects.map((project) => (
@@ -74,6 +74,15 @@ export default function Example() {
                             >
                                 {project.status}
                             </p>
+                        </div>
+                        <div className="mt-1 flex items-center gap-x-2 text-xs leading-5 text-gray-500">
+                            <p className="whitespace-nowrap">
+                                Assignee:
+                            </p>
+                            {/* <svg viewBox="0 0 2 2" className="h-0.5 w-0.5 fill-current">
+                                <circle r={1} cx={1} cy={1} />
+                            </svg> */}
+                            <p className="truncate">Robert</p>
                         </div>
                         <div className="mt-1 flex items-center gap-x-2 text-xs leading-5 text-gray-500">
                             <p className="whitespace-nowrap">
